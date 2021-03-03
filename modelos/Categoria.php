a645a2e6999320f677ee8e5d40c718b99e20d0d8
Ariel <ArielR135@outlook.com>
2021-03-03 17:45:13 -0300

    poo mejorado en clase Categoria

--- modelos/Categoria.php
+++ modelos/Categoria.php
@@ -4,61 +4,60 @@ require "../config/Conexion.php";
 
 Class Categoria {
 
+	private $idcategoria;
 	private $nombre;
 	private $descripcion;
 
 	//Implementamos nuestro constructor
-	function __construct($nombre=null, $descripcion=null, $estado=null) {
+	function __construct($idcategoria=null, $nombre=null, $descripcion=null, $estado=null) {
+		$this->idcategoria = $idcategoria;
 		$this->nombre = $nombre;
 		$this->descripcion = $descripcion;
 		$this->estado = $estado;
 	}
 
-	function setNombre($nombre) {
-		$this->nombre = $nombre;
-	}
-	function setDescripcion($descripcion) {
-		$this->descripcion = $descripcion;
+	function getIdCategoria() {
+		return $this->idcategoria;
 	}
 
 	//Implementamos un método para insertar registros
-	public function insertar() {
+	function insertar() {
 		$sql = "INSERT INTO categorias (nombre, descripcion, estado) VALUES ('$this->nombre', '$this->descripcion', '1')";
 		return ejecutarConsulta($sql);
 	}
 
 	//Implementamos un método para editar registros
-	public function editar($idcategoria) {
-		$sql = "UPDATE categorias SET nombre='$this->nombre', descripcion='$this->descripcion' WHERE idcategoria='$idcategoria'";
+	function editar() {
+		$sql = "UPDATE categorias SET nombre='$this->nombre', descripcion='$this->descripcion' WHERE idcategoria='$this->idcategoria'";
 		return ejecutarConsulta($sql);
 	}
 
 	//Implementamos un método para desactivar registros
-	public function desactivar($idcategoria) {
-		$sql = "UPDATE categorias SET estado='0' WHERE idcategoria='$idcategoria'";
+	function desactivar() {
+		$sql = "UPDATE categorias SET estado='0' WHERE idcategoria='$this->idcategoria'";
 		return ejecutarConsulta($sql);
 	}
 
 	//Implementamos un método para activar registros
-	public function activar($idcategoria) {
-		$sql = "UPDATE categorias SET estado='1' WHERE idcategoria='$idcategoria'";
+	function activar() {
+		$sql = "UPDATE categorias SET estado='1' WHERE idcategoria='$this->idcategoria'";
 		return ejecutarConsulta($sql);
 	}
 
 	//Implementar un método para mostrar los datos de un registro a modificar
-	public function mostrar($idcategoria) {
-		$sql = "SELECT * FROM categorias WHERE idcategoria='$idcategoria'";
+	function mostrar() {
+		$sql = "SELECT * FROM categorias WHERE idcategoria='$this->idcategoria'";
 		return ejecutarConsultaSimpleFila($sql);
 	}
 
 	//Implementar un método para listar los registros
-	public function listar() {
+	static function listar() {
 		$sql = "SELECT * FROM categorias";
 		return ejecutarConsulta($sql);
 	}
 
 	//Implementar un método para listar los registros y mostrar en el select
-	public function select() {
+	static function select() {
 		$sql="SELECT * FROM categorias where estado=1";
 		return ejecutarConsulta($sql);		
 	}
